f0e9c0b90c743603639bbf1699b30b6c1299797c
May Myat Mon <maymyatmon@BIMs-MacBook-Pro-2.local>
2022-05-17 17:33:22 +0630

    strapi promotion api

---
+++ src/api/promotion/controllers/promotion.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ *  promotion controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::promotion.promotion', ({ strapi }) => ({
+    async find(ctx) {
+        let status = "success";
+        
+        const { data, meta } = await super.find(ctx);
+        //return data;
+        let param = ctx.request.querystring;
+        console.log(param);
+        let isInclude = param.includes("populate=deep");
+
+        if (isInclude) {
+            let responseMap = [];
+            data.map((value, index) => {
+                let photoArr = value.attributes.photo_path.data;
+                let photo = photoArr.slice(0, 1).shift();
+                responseMap.push({
+                    "promotion_id": value.id,
+                    "photo_path": photo.attributes.url,
+                    "promotion_type": value.attributes.promotion_type.data.attributes.type_title,
+                    "category_id": value.attributes.categoriess.data.id,
+                    "hasDetails": value.attributes.hasDetails,
+                    "category_title":value.attributes.categoriess.data.attributes.category_title,
+                    "action_link_ios": value.attributes.action_link_ios,
+                    "action_link_android": value.attributes.action_link_android,
+                    'promotion_title': value.attributes.promotion_title,
+                });
+            });
+            return { status, responseMap };
+
+        }
+    }
+}));
--- src/api/single-category/controllers/single-category.js
+++ src/api/single-category/controllers/single-category.js
@@ -8,38 +8,40 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::single-category.single-category', ({ strapi }) => ({
     async find(ctx) {
-        let status ="success";
+
+        let status = "success";
         const { data, meta } = await super.find(ctx);
+        //return data;
         console.log(ctx.request.querystring);
         let param = ctx.request.querystring;
         let isInclude = param.includes("populate=deep");
         console.log(isInclude);
-        if(isInclude){
+        if (isInclude) {
             let responseMap = [];
-        data.map((value, index) => {
-            let imageArr = value.attributes.image;
+            data.map((value, index) => {
+                let imageArr = value.attributes.image;
 
-            imageArr.map((imageValue, imageIndex) => {
-                let photoArr = imageValue.photo_path.data;
-                let photo = photoArr.attributes.url;
-                responseMap.push({
-                    'category_id': imageValue.id,
-                    'category_title': imageValue.category_title,
-                    'photo_path': photoArr.attributes.url
+                imageArr.map((imageValue, imageIndex) => {
+                    let photoArr = imageValue.photo_path.data;
+                    let photo = photoArr.attributes.url;
+                    responseMap.push({
+                        'category_id': imageValue.id,
+                        'category_title': imageValue.category_title,
+                        'photo_path': photoArr.attributes.url
 
+                    });
+                    //console.log(imageValue, "image value");
+                    //console.log(photoArr.attributes.url);
                 });
-                //console.log(imageValue, "image value");
-                //console.log(photoArr.attributes.url);
+
             });
-            
-        });
-        return {status,responseMap};
-            
+            return { status, responseMap };
+
         }
         return data;
 
 
-        
+
     },
 
 
