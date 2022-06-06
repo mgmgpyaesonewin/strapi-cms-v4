106b85ec83cecc47f2aa4304842ec8c974741871
May Myat Mon <maymyatmon@BIMs-MacBook-Pro-2.local>
2022-06-06 11:18:26 +0630

    language mapping in category

--- src/api/categoriess/controllers/categoriess.js
+++ src/api/categoriess/controllers/categoriess.js
@@ -10,17 +10,16 @@ module.exports = createCoreController('api::categoriess.categoriess', ({ strapi 
 
 
   async find(ctx) {
-
     let status = 'success';
     ctx.query = { ...ctx.query, local: 'en' }
     const { data, meta } = await super.find(ctx);
     const entries = await strapi.entityService.findMany('api::categoriess.categoriess', {
       populate: 'deep',
-      //sort: {position: 'asc'},
-      locale: ctx.query.locale
+      locale: (ctx.query.locale) ? ((ctx.query.locale == 'zw') ? ("my-MM") : (ctx.query.locale)) : 'en'
+      // (condition1) ? ((condition2) ? (true block2)  : (else block2) ) : (else block1)
     });
 
-      let responseMap = entries.map((value, index) => {
+    let responseMap = entries.map((value, index) => {
       let photoNormalArr = value.photo_path_normal;
       let photoNormal = photoNormalArr.slice(0, 1).shift();
 
@@ -29,7 +28,6 @@ module.exports = createCoreController('api::categoriess.categoriess', ({ strapi 
 
       return {
         'category_id': value.category_id,
-      
         'category_title': value.category_title,
         'photo_path_normal': photoNormal.url,
         'photo_path_selected': photoSelected.url,
@@ -41,5 +39,6 @@ module.exports = createCoreController('api::categoriess.categoriess', ({ strapi 
 
 
   },
+
 
 }));
