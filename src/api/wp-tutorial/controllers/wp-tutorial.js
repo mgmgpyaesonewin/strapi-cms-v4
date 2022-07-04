54c3280efa06482dc7dc6b2ab9da797b55ff5249
May Myat Mon <maymyatmon@BIMs-MacBook-Pro-2.local>
2022-07-04 11:26:04 +0630

    refactor: 💡 refactor wp-tutorial detail

--- src/api/wp-tutorial/controllers/wp-tutorial.js
+++ src/api/wp-tutorial/controllers/wp-tutorial.js
@@ -10,29 +10,7 @@ module.exports = createCoreController('api::wp-tutorial.wp-tutorial', ({ strapi 
     async findOne(ctx) {
 
         const { id } = ctx.params;
-
-        const entity = await strapi.db.query('api::wp-tutorial.wp-tutorial').findOne({
-            where: { id: id },
-            select: ['name'],
-            populate: {
-                title: true,
-                description: true,
-                ["image"]: {
-                    select: ["url"],
-                },
-                ["tutorials"]: {
-                    populate: {
-                        title: true,
-                        description: true,
-                        ["image"]: {
-                            select: ["url"],
-                        },
-                    },
-
-                }
-            }
-
-        });
+        const entity = await strapi.service('api::wp-tutorial.wp-tutorial').findOne(id);
         return entity;
     }
 }));
--- src/api/wp-tutorial/services/wp-tutorial.js
+++ src/api/wp-tutorial/services/wp-tutorial.js
@@ -6,4 +6,35 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::wp-tutorial.wp-tutorial');
+module.exports = createCoreService('api::wp-tutorial.wp-tutorial', ({ strapi }) => ({
+
+    async findOne(id) {
+
+        const entity = await strapi.db.query('api::wp-tutorial.wp-tutorial').findOne({
+            where: { id: id },
+            select: ['name'],
+            populate: {
+                title: true,
+                description: true,
+                ["image"]: {
+                    select: ["url"],
+                },
+                ["tutorials"]: {
+                    populate: {
+                        title: true,
+                        description: true,
+                        ["image"]: {
+                            select: ["url"],
+                        },
+                    },
+
+                }
+            }
+
+        });
+        return entity;
+
+    }
+
+
+}));
