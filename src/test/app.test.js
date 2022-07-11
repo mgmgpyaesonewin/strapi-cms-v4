d4e352395ffd79af1de33d45513407b8df7ea44e
May Myat Mon <maymyatmon@BIMs-MacBook-Pro-2.local>
2022-07-11 10:58:50 +0630

    test

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,45 +1,11 @@
-// const fs = require('fs');
-// const { setupStrapi } = require('./helpers/strapi');
-//
-// /** this code is called once before any test */
-// beforeAll(async done => {
-//   await setupStrapi();
-//   done();
-// });
-//
-// /** this code is called after all the tests are finished */
-// afterAll(done => {
-//   const dbSettings = strapi.config.get('database.connections.default.settings');
-//   //console.log(dbSettings);
-//   done();
-// });
-//
-// it('strapi is defined', () => {
-//   expect(strapi).toBeDefined();
-// });
-// jest.setTimeout(30000)
+const {setupStrapi, teardownStrapi} = require('./helpers/strapi')
 
-const fs = require('fs');
-const { setupStrapi } = require('./helpers/strapi');
-/** this code is called once before any test */
-beforeAll(async done => {
-  await setupStrapi();
-  done();
-});
-/** this code is called after all the tests are finished */
-afterAll(async done => {
-  const dbSettings = strapi.config.get('database.connections.default.settings');
-  //This will delete test database after all tests
-  if (dbSettings && dbSettings.filename) {
-    const tmpDbFile = `${__dirname}/../${dbSettings.filename}`;
-    if (fs.existsSync(tmpDbFile)) {
-      fs.unlinkSync(tmpDbFile);
-    }
-  }
-  done();
-});
-//Run test to make sure Strapi is defined.
-it('strapi is defined', () => {
-  expect(strapi).toBeDefined();
-});
+/** this code is called once before any test is called */
+beforeAll(setupStrapi);
 
+/** this code is called once before all the tested are finished */
+afterAll(teardownStrapi);
+
+it('strapi instance is defined', () => {
+  expect(global.strapiInstance).toBeDefined();
+});
--- src/test/helpers/strapi.js
+++ src/test/helpers/strapi.js
@@ -1,26 +1,25 @@
-const Strapi = require("@strapi/strapi");
-// let instance;
-//
-// async function setupStrapi() {
-//   if (!instance) {
-//     await Strapi().start();
-//     instance = strapi;
-//   }
-//   return instance;
-// }
-const http = require('http');
-let instance;
-async function setupStrapi() {
-  //console.log("here");
-  if (!instance) {
-    /** the following code is copied from `./node_modules/strapi/lib/Strapi.js` */
-    await Strapi().load();
-    instance = strapi; // strapi is now global
-    await instance.app
-      .use(instance.router.routes()) // populate KOA routes
-      .use(instance.router.allowedMethods()); // populate KOA methods
-    instance.server = http.createServer(instance.app.callback());
-  }
-  return instance;
+const strapi = require('@strapi/strapi')
+const fs = require('fs');
+
+
+const setupStrapi = async () => {
+  global.strapiInstance = await strapi().load();
+  global.strapiInstance.server.mount()
+  global.strapiServer = global.strapiInstance.server.app.callback();
 }
-module.exports = { setupStrapi };
+
+const teardownStrapi = () => {
+  global.strapiInstance.destroy().then(() => {
+    //delete test database after all tests
+    const dbSettings = global.strapiInstance.config.get('database.connections.default.settings');
+    if (dbSettings && dbSettings.filename) {
+      const tmpDbFile = `${__dirname}/../${dbSettings.filename}`;
+      if (fs.existsSync(tmpDbFile)) {
+        fs.unlinkSync(tmpDbFile);
+      }
+    }
+  })
+}
+
+
+module.exports = {setupStrapi, teardownStrapi}
