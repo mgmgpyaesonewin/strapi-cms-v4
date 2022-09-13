3281094d494db1a58af4593346e4bb01537a610b
May Myat Mon <maymyatmon@BIMs-MacBook-Pro-2.local>
2022-09-13 15:32:13 +0630

    feat: 🎸 remove common app error mapping collection&componment
    Remove common app error schema,route,controller,service, wp error
    componment,wc error componment

--- src/api/app-error/routes/app-error.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * app-error router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::app-error.app-error');
