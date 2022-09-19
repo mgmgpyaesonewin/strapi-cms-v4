6a88e6cbcd197f3edbadbdf67688453dc7c0f1e3
May Myat Mon <maymyatmon@BIMs-MacBook-Pro-2.local>
2022-09-19 15:11:02 +0630

    feat: 🎸 separate wp-dynamic-api for misfit
    ✅ Closes: CMS-126

--- src/api/wp-dynamic-list/controllers/wp-dynamic-list.js
+++ src/api/wp-dynamic-list/controllers/wp-dynamic-list.js
@@ -15,7 +15,13 @@ module.exports = createCoreController('api::wp-dynamic-list.wp-dynamic-list', ({
         let merged = {...dynamicList, ...region,...occupation};
         return merged;
 
+    },
+    async dynamicList(ctx) {
+        let region = await strapi.controller('api::config-region.config-region').addressForWP(ctx);
+        let dynamicList = await strapi.service('api::wp-dynamic-list.wp-dynamic-list').findConfig(ctx);
+        let occupation = await strapi.controller('api::wp-occupation-list.wp-occupation-list').find(ctx);
+        let merged = {...dynamicList, ...region,...occupation};
+        return merged;
     }
         
-
 }));
