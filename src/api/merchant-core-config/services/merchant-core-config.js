49c91a1c5ee26fcf8ec68f2aa23e2c5b6f2b6b1c
May Myat Mon <maymyatmon@BIMs-MacBook-Pro-2.local>
2022-09-21 17:37:28 +0630

    feat: 🎸 version upgrade to 4.2.2

--- src/api/merchant-core-config/services/merchant-core-config.js
+++ src/api/merchant-core-config/services/merchant-core-config.js
@@ -9,7 +9,15 @@ const {createCoreService} = require('@strapi/strapi').factories;
 module.exports = createCoreService('api::merchant-core-config.merchant-core-config', ({strapi}) => ({
   async find(ctx) {
     const entriesCoreConfig = await strapi.entityService.findMany('api::merchant-core-config.merchant-core-config', {
-      populate: 'deep',
+      populate: {
+        config: {
+         populate: {
+            theme: true,
+            header_logo: true,
+            footer_logo:true
+          },
+        },
+      },
       publicationState: 'live',
     });
     return entriesCoreConfig;
--- src/api/merchant-login/services/merchant-login.js
+++ src/api/merchant-login/services/merchant-login.js
@@ -4,12 +4,48 @@
  * merchant-login service.
  */
 
-const {createCoreService} = require('@strapi/strapi').factories;
+const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::merchant-login.merchant-login', ({strapi}) => ({
+module.exports = createCoreService('api::merchant-login.merchant-login', ({ strapi }) => ({
   async find(ctx) {
     const login = await strapi.entityService.findMany('api::merchant-login.merchant-login', {
-      populate: 'deep',
+      // populate: ['validations_create_pin','validations_msisdn','allowedLogin','signup_confirm_pin','password_guide_dialog','validations_confirm_pin'],
+      populate: {
+        validations_create_pin: {
+          populate: {
+            error_message: true
+          }
+        },
+        validations_msisdn: {
+          populate: {
+            error_message: true
+          }
+        },
+        allowedLogin: true,
+        signup_confirm_pin: {
+          populate: {
+            html_text: true,
+          },
+        },
+        password_guide_dialog:{
+          populate:{
+            title:true,
+            subtitle:true,
+            rules:{
+              populate:{
+                title:true,
+                items:true
+              }
+            }
+
+          }
+        },
+        validations_confirm_pin:{
+          populate:{
+            error_message:true
+          }
+        }
+      },
       publicationState: 'live',
     });
     return login;
