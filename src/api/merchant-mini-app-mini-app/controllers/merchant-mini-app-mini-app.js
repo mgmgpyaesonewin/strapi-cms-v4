82b0858b959781e7e0939d987df641b928b1f86b
maymyat34 <105039466+maymyat34@users.noreply.github.com>
2022-09-23 15:03:27 +0630

    Revert "Revert "feat: 🎸 merchant mini-app and mini-app-category""

---
+++ src/api/merchant-mini-app-mini-app/controllers/merchant-mini-app-mini-app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  merchant-mini-app-mini-app controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::merchant-mini-app-mini-app.merchant-mini-app-mini-app');
