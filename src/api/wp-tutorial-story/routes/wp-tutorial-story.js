ff102abd956dec833e8d09f13458fa1ac299826d
maymyat <maymyat.mon@wavemoney.com.mm>
2022-09-29 13:17:03 +0630

    feat: 🎸 Fetch Story Detail by Story Detail Name
    story detail name - unique, fetch by name
    
    ✅ Closes: CMS-143

--- src/api/wp-tutorial-story/routes/wp-tutorial-story.js
+++ src/api/wp-tutorial-story/routes/wp-tutorial-story.js
@@ -6,4 +6,32 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::wp-tutorial-story.wp-tutorial-story');
+const defaultRouter= createCoreRouter('api::wp-tutorial-story.wp-tutorial-story');
+const customRouter = (innerRouter, extraRoutes = []) => {
+    let routes;
+    return {
+      get prefix() {
+        return innerRouter.prefix;
+      },
+      get routes() {
+        if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+        return routes;
+      },
+    };
+  };
+  
+  const myExtraRoutes = [
+    {
+      method: 'GET',
+      path: "/wp-tutorial-stories/name/:name",
+      handler: 'wp-tutorial-story.getbyName',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ];
+  
+  module.exports = customRouter(defaultRouter, myExtraRoutes);
+  
+  
