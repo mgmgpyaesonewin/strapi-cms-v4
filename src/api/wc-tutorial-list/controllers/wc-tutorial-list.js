6e466bc6f6681699fd298b3fae5bf1adc32ece4f
maymyat <maymyat.mon@wavemoney.com.mm>
2022-10-18 17:36:28 +0630

    feat: 🎸 wc standard tutorial

--- src/api/wc-tutorial-list/controllers/wc-tutorial-list.js
+++ src/api/wc-tutorial-list/controllers/wc-tutorial-list.js
@@ -6,4 +6,11 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::wc-tutorial-list.wc-tutorial-list');
+module.exports = createCoreController('api::wc-tutorial-list.wc-tutorial-list', ({strapi}) => ({
+    async find(ctx) {
+      let stories= await strapi.service('api::wc-story-list.wc-story-list').find(ctx);
+      return {
+        data: stories
+      };
+    }
+  }));
--- src/api/wc-tutorial-list/services/wc-tutorial-list.js
+++ src/api/wc-tutorial-list/services/wc-tutorial-list.js
@@ -6,4 +6,55 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::wc-tutorial-list.wc-tutorial-list');
+module.exports = createCoreService('api::wc-tutorial-list.wc-tutorial-list', ({ strapi }) => ({
+    async find(ctx) {
+      const entityStoryLists = await strapi.db.query('api::wc-story-list.wc-story-list').findMany({
+        select: ['id', 'type', 'story_name', 'position'],
+        orderBy: { position: 'asc' },
+        where: {
+          $and: [
+            {
+              publishedAt: {
+                $notNull: true,
+              }
+            },
+            {
+                wc_story_contents: {
+                publishedAt: {
+                  $notNull: true,
+                }
+              }
+            }
+          ]
+        },
+        populate: {
+          ["image"]: {
+            select: ["url"],
+          },
+          title: true,
+          description: true,
+          ["wc_story_contents"]: {
+            orderBy: { position: 'asc' },
+            where: {
+              publishedAt: {
+                $notNull: true,
+              },
+            },
+            populate: {
+              title: true,
+              description: true,
+              ["image"]: {
+                select: ["url"],
+              },
+  
+            }
+          }
+  
+        },
+  
+      });
+  
+      return entityStoryLists;
+    }
+  
+  }));
