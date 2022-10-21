6845a63ba5e1c32989753a6d47a983e87ead0211
maymyat <maymyat.mon@wavemoney.com.mm>
2022-10-21 17:18:19 +0630

    feat: 🎸 parse data on audit log

--- config/function/parseAuditLogs.js
+++ config/function/parseAuditLogs.js
@@ -1,34 +1,50 @@
 module.exports = async () => {
-    try {
+  try {
 
-        console.log("cron " + new Date().toISOString());
-        const date = new Date();
-        const todayDate = new Date();
-        const additionOfMonths = 2;
-        date.setMonth(date.getMonth() - additionOfMonths); // For subtract use minus (-)
-       // date.setDay(date.getDay() - additionOfMonths); // For subtract use minus (-)
-        console.log('New Date:', date);
-        const entries = await strapi.entityService.findMany('api::trail.trail', {
-            filters: {
-              $and: [
-                {
-                    createdAt: {
-                        $gte: date,
-                      },
-                },
-                {
-                    createdAt: {
-                        $lte: todayDate,
-                      },
-                },
-              ],
+    console.log("cron " + new Date());
+    const subtract = new Date();
+    const todayDate = new Date();
+    const additionOfMonths = 3;
+    subtract.setMonth(subtract.getMonth() - additionOfMonths); // For subtract use m
+    console.log('New Date:', subtract);
+    // const entries = await strapi.entityService.findMany('api::trail.trail', {
+    //     filters: {
+    //       $and: [
+    //         {
+    //             createdAt: {
+    //                 $gte: subtract,
+    //               },
+    //         },
+    //         {
+    //             createdAt: {
+    //                 $lte: todayDate,
+    //               },
+    //         },
+    //       ],
+    //     },
+    //   });
+    const entries = await strapi.db.query('api::trail.trail').deleteMany({
+      where: {
+        $and: [
+          {
+            createdAt: {
+              $gte: subtract,
             },
-          });
-          
-          console.log(entries,entries.length);
-        //await strapi.entityService.delete('api::trail.trail', 1);
-        //await strapi.entityService.delete('api::trail.trail', 1);
-    } catch (error) {
-        console.warm(error);
-    }
+          },
+          {
+            createdAt: {
+              $lte: todayDate,
+            },
+          },
+        ],
+      },
+    });
+
+
+    console.log(entries.length);
+    //await strapi.entityService.delete('api::trail.trail', 1);
+    //await strapi.entityService.delete('api::trail.trail', 1);
+  } catch (error) {
+    console.warm(error);
+  }
 };
