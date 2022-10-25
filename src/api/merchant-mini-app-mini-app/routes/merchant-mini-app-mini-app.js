171b881b5652e56d6640ff74619c53060a1dd476
maymyat <maymyat.mon@wavemoney.com.mm>
2022-10-25 15:11:09 +0630

    feat: 🎸 wave pay 1.4.2 release prepare
    remove
    wc-localization,wc-tutorial-list,wc-tutorial-subcategory,wp-dynamic-list,wp-home,wp-localization_content(ios,android)
    
    BREAKING CHANGE: 🧨 wp-tutorial,wp-home,wp-dynamic-list,wp-localization-content,merchant-mini-app
    remove

--- src/api/merchant-mini-app-mini-app/routes/merchant-mini-app-mini-app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * merchant-mini-app-mini-app router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::merchant-mini-app-mini-app.merchant-mini-app-mini-app');
--- src/api/wc-tutorial-content/routes/wc-tutorial-content.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * wc-tutorial-content router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::wc-tutorial-content.wc-tutorial-content');
