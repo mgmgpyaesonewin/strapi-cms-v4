19e6d278a12a66c5802a751edcdd16b25eeab1ca
maymyat34 <105039466+maymyat34@users.noreply.github.com>
2022-10-28 10:56:29 +0630

    Revert "Revert "Feature/merchant/2022 oct/force upgrade""

---
+++ src/api/merchant-ios-app-version-info/controllers/merchant-ios-app-version-info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * merchant-ios-app-version-info controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::merchant-ios-app-version-info.merchant-ios-app-version-info');
---
+++ src/api/merchant-version-info-android/controllers/merchant-version-info-android.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * merchant-version-info-android controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::merchant-version-info-android.merchant-version-info-android');
