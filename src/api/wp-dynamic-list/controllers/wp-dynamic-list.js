4c043814ba5dee98d334e806bd26ec0b29911998
maymyat <maymyat.mon@wavemoney.com.mm>
2022-12-15 14:31:02 +0630

    feat: 🎸 remove occupation add in dynamic list
    remove occupation list and added as json in dynamic list

--- src/api/wp-dynamic-list/controllers/wp-dynamic-list.js
+++ src/api/wp-dynamic-list/controllers/wp-dynamic-list.js
@@ -11,16 +11,14 @@ module.exports = createCoreController('api::wp-dynamic-list.wp-dynamic-list', ({
     async find(ctx) {
         let region = await strapi.controller('api::config-region.config-region').addressForWP(ctx);
         let dynamicList = await strapi.service('api::wp-dynamic-list.wp-dynamic-list').find(ctx);
-        let occupation = await strapi.controller('api::wp-occupation-list.wp-occupation-list').find(ctx);
-        let merged = {...dynamicList, ...region,...occupation};
+        let merged = {...dynamicList, ...region};
         return merged;
 
     },
     async dynamicList(ctx) {
         let region = await strapi.controller('api::config-region.config-region').addressForWP(ctx);
         let dynamicList = await strapi.service('api::wp-dynamic-list.wp-dynamic-list').findConfig(ctx);
-        let occupation = await strapi.controller('api::wp-occupation-list.wp-occupation-list').find(ctx);
-        let merged = {...dynamicList, ...region,...occupation};
+        let merged = {...dynamicList, ...region};
         return merged;
     }
         
