6bc853c071f5c32cc18ec931a3c24ed1f5317593
maymyat <maymyat.mon@wavemoney.com.mm>
2023-02-08 14:37:07 +0630

    feat: 🎸 call MW when nrc change

--- src/api/config-nrc/content-types/config-nrc/lifecycles.js
+++ src/api/config-nrc/content-types/config-nrc/lifecycles.js
@@ -1,9 +1,19 @@
+const axios = require('axios');
 module.exports = {
     afterUpdate(event) {
         const { result, params, data } = event;
-        console.log('---------');
-        console.log("after update", result.nrc);
-        console.log('---------');
+        axios.post(process.env.MW_NRC_URL, {
+            "nrcJsonString": JSON.stringify(result.nrc),
+        }, {
+            headers: {
+                'client_id': process.env.MW_CLIENT_ID,
+                'client_secret': process.env.MW_CLIENT_SECRET
+            }
+        }).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        });
 
     },
 };
