f778ed6d8da19cb0a685627327be28f587ce2525
maymyat <maymyat.mon@wavemoney.com.mm>
2023-04-26 10:10:22 +0630

    feat: 🎸 sorting district and township by code

--- src/api/config-township/services/config-township.js
+++ src/api/config-township/services/config-township.js
@@ -7,55 +7,65 @@
 const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::config-township.config-township', ({ strapi }) => ({
-    async find(ctx) {
-      return await strapi.db.query('api::config-township.config-township').findMany({
-        populate: {
-          title: true,
-          ["city_district"]: {
-            populate: {
-              title: true,
-            },
-            where: {
-              publishedAt: {
-                $notNull: true,
-              },
-            },
-            select: ['name', 'code']
-          }
-  
-        },
-        where: {
-          $and: [
-            {
-              publishedAt: {
-                $notNull: true,
-              },
-            },
-            {
-              city_district: {
-                publishedAt: {
-                  $notNull: true,
-                }
-              },
-            }
-          ],
-        },
-        select: ['name', 'code']
-      });
-    },
-    async findByService(ctx) {
-      return await strapi.entityService.findMany('api::config-township.config-township', {
-        populate: ['city_district', 'title'],
-        publicationState: 'live',
-        filters: {
-          city_district: {
-            id: {
+  async find(ctx) {
+    return await strapi.db.query('api::config-township.config-township').findMany({
+      populate: {
+        title: true,
+        ["city_district"]: {
+          populate: {
+            title: true,
+          },
+          where: {
+            publishedAt: {
               $notNull: true,
-            }
-          }
-  
+            },
+          },
+          select: ['name', 'code']
         }
-      });
-    }
-  }));
-  
+
+      },
+      where: {
+        $and: [
+          {
+            publishedAt: {
+              $notNull: true,
+            },
+          },
+          {
+            city_district: {
+              publishedAt: {
+                $notNull: true,
+              }
+            },
+          }
+        ],
+      },
+      orderBy: [
+        {
+          city_district: {
+            code: 'asc',
+          },
+        },
+        {
+          name: 'asc',
+        }
+      ],
+      select: ['name', 'code']
+    });
+  },
+  async findByService(ctx) {
+    return await strapi.entityService.findMany('api::config-township.config-township', {
+      populate: ['city_district', 'title'],
+      publicationState: 'live',
+      filters: {
+        city_district: {
+          id: {
+            $notNull: true,
+          }
+        }
+
+      }
+    });
+  }
+}));
+
