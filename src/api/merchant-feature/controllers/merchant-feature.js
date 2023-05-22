8e6516894d37e3773a83dd0e396d52348b0a94bd
maymyat <maymyat.mon@wavemoney.com.mm>
2023-05-22 14:05:53 +0630

    feat: 🎸 localization with feature

---
+++ src/api/merchant-feature/controllers/merchant-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * merchant-feature controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::merchant-feature.merchant-feature');
