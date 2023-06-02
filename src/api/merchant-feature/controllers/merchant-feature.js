fa816c6e9d8144c638bb5c6c5b75d756450b7539
maymyat <maymyat.mon@wavemoney.com.mm>
2023-06-02 13:47:18 +0630

    feat: 🎸 merchant feature bulk upload api

--- src/api/merchant-feature/controllers/merchant-feature.js
+++ src/api/merchant-feature/controllers/merchant-feature.js
@@ -6,4 +6,13 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::merchant-feature.merchant-feature');
+module.exports = createCoreController('api::merchant-feature.merchant-feature', ({ strapi }) => ({
+    async findFeature(ctx) {
+        const { feature } = ctx.params;
+        let content = await strapi.service('api::merchant-feature.merchant-feature').findByFeature(feature);
+        console.log(content);
+        return {
+            'contents': content
+        }
+     }
+}));
--- src/api/merchant-feature/routes/merchant-feature.js
+++ src/api/merchant-feature/routes/merchant-feature.js
@@ -6,4 +6,28 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::merchant-feature.merchant-feature');
+const defaultRouter = createCoreRouter('api::merchant-feature.merchant-feature');
+const customRouter = (innerRouter, extraRoutes = []) => {
+    let routes;
+    return {
+        get prefix() {
+            return innerRouter.prefix;
+        },
+        get routes() {
+            if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+            return routes;
+        },
+    };
+};
+const myExtraRoutes = [
+    {
+        method: 'GET',
+        path: '/wp-features/feature/:feature',
+        handler: 'wp-feature.findFeature',
+        config: {
+            policies: [],
+            middlewares: [],
+        },
+    },
+];
+module.exports = customRouter(defaultRouter, myExtraRoutes);
