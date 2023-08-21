945f8ec1d17018f2c883c08a861ca8c71a3178cb
May Myat Mon <maymyatmon@DMM-Mac14GN2C4Y.local>
2023-08-21 15:02:10 +0630

    feature:wc otc feature flag
    https://wavemoneytransform.atlassian.net/browse/B2BCHA-1742

---
+++ src/api/wc-otc-profiling-flag/routes/wc-otc-profiling-flag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * wc-otc-profiling-flag router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::wc-otc-profiling-flag.wc-otc-profiling-flag');
