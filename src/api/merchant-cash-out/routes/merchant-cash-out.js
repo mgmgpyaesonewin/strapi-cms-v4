faccf3ea379604b11f8f78cf6d49aef9403a772c
eizin <eizin.htun@wavemoney.com.mm>
2023-12-19 11:22:19 +0630

    add expired_duration on core config

--- src/api/merchant-cash-out/routes/merchant-cash-out.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * merchant-cash-out router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::merchant-cash-out.merchant-cash-out');
