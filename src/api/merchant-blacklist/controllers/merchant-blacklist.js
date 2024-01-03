577ed0386df541afaebae61b6c515f683ed621a9
May Myat Mon <maymyatmon@DMM-Mac14GN2C4Y.local>
2024-01-03 12:50:16 +0630

    feature:black-list-config

---
+++ src/api/merchant-blacklist/controllers/merchant-blacklist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * merchant-blacklist controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::merchant-blacklist.merchant-blacklist');
