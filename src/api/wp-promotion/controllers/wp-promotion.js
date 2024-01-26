d0870ce3d6a27e7eb2e2938ff39e984307208627
Kaung Khant Htun <kaungkhant.hutn@wavemoney.com.mm>
2024-01-26 13:29:03 +0630

    modify promotions

--- src/api/wp-promotion/controllers/wp-promotion.js
+++ src/api/wp-promotion/controllers/wp-promotion.js
@@ -13,17 +13,17 @@ module.exports = createCoreController('api::wp-promotion.wp-promotion', ({strapi
     let status = "Success";
     let types = ['NORMAL', 'SPECIAL'];
     let promotionType = ctx.query.promotionType;
-    const promotions = await strapi.service('api::wp-promotion.wp-promotion').find(ctx);
+    const promotionList = await strapi.service('api::wp-promotion.wp-promotion').find(ctx);
     var finalData;
     if (types.includes(ctx.query.promotionType)) {
-      finalData = promotions.filter(function (filterType) {
+      finalData = promotionList.filter(function (filterType) {
         return filterType.wp_promotion_type.type_title == ctx.query.promotionType;
       });
     } else {
-      finalData = promotions;
+      finalData = promotionList;
     }
-    let responseMap = responseMapping(finalData,ctx.request.header);
-    return {status, responseMap};
+    let promotions = responseMapping(finalData,ctx.request.header);
+    return {status, promotions};
   },
 
   async filterByCategory(ctx, next) {
@@ -31,6 +31,7 @@ module.exports = createCoreController('api::wp-promotion.wp-promotion', ({strapi
     let {id} = ctx.params;
     const promotions = await strapi.service('api::wp-promotion.wp-promotion').filterByCategoryID(id);
     let responseMap = responseMapping(promotions,ctx.request.header);
+    
     return {status, responseMap};
   },
 
@@ -68,21 +69,31 @@ function responseMapping(promotionEntries,requestHeader) {
   let mapping = [];
   promotionEntries.map((value, index) => {
     mapping.push({
-      "promotion_id": value.id,
-      'promotion_title': value.promotion_title,
+      "id": value.id,
+      "position": value.position,
+      "title": value.title,
       "photo_path": value.photo_path ? value.photo_path.url : '',
       "promotion_type": !value.wp_promotion_type ? '' : value.wp_promotion_type.type_title,
-      "category_id": !value.wp_category ? '' : value.wp_category.id,
-      "category_title": !value.wp_category ? '' : value.wp_category.category_title,
+      "category": !value.wp_category ? {} : value.wp_category,
+      // "category_id": !value.wp_category ? '' : value.wp_category.id,
+      // "category_title": !value.wp_category ? '' : value.wp_category.category_title,
+      "include_header": value.include_header,
+      "is_login": value.is_login,
+      "display": value.display,
+      "kyc_level_check":value.kyc_level_check,
+      "deep_link": value.wp_deeplink, 
+      "feature_id": !value.wp_feature_id ? null : value.wp_feature_id.feature_id, 
       "hasDetails": value.hasDetails,
       "action_link_ios": value.action_link_ios,
       "action_link_android": requestHeader.versioncode ? value.new_action_link_android : value.action_link_android,
-      "position": value.position,
       "promotion_code": value.promotion_code,
       "is_external": value.is_external,
       "external_deeplink": value.external_deeplink,
       "promotion_details": value.promotion_details,
-      "kyc_level_check":value.kyc_level_check
+      "paths": value.paths,
+      "parameters": value.parameters,
+      "widget_id": !value.wp_home_widget ? null : value.wp_home_widget.id,
+      
     });
   });
   return mapping;
