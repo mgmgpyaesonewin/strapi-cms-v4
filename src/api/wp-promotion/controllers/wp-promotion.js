f4f921946238730c01b3e8a51964ff9c7e53bb7e
Kaung Khant Htun <kaungkhant.hutn@wavemoney.com.mm>
2024-01-26 14:50:51 +0630

    Revert "modify promotions"
    This reverts commit d0870ce3d6a27e7eb2e2938ff39e984307208627.

--- src/api/wp-promotion/controllers/wp-promotion.js
+++ src/api/wp-promotion/controllers/wp-promotion.js
@@ -13,17 +13,17 @@ module.exports = createCoreController('api::wp-promotion.wp-promotion', ({strapi
     let status = "Success";
     let types = ['NORMAL', 'SPECIAL'];
     let promotionType = ctx.query.promotionType;
-    const promotionList = await strapi.service('api::wp-promotion.wp-promotion').find(ctx);
+    const promotions = await strapi.service('api::wp-promotion.wp-promotion').find(ctx);
     var finalData;
     if (types.includes(ctx.query.promotionType)) {
-      finalData = promotionList.filter(function (filterType) {
+      finalData = promotions.filter(function (filterType) {
         return filterType.wp_promotion_type.type_title == ctx.query.promotionType;
       });
     } else {
-      finalData = promotionList;
+      finalData = promotions;
     }
-    let promotions = responseMapping(finalData,ctx.request.header);
-    return {status, promotions};
+    let responseMap = responseMapping(finalData,ctx.request.header);
+    return {status, responseMap};
   },
 
   async filterByCategory(ctx, next) {
@@ -31,7 +31,6 @@ module.exports = createCoreController('api::wp-promotion.wp-promotion', ({strapi
     let {id} = ctx.params;
     const promotions = await strapi.service('api::wp-promotion.wp-promotion').filterByCategoryID(id);
     let responseMap = responseMapping(promotions,ctx.request.header);
-    
     return {status, responseMap};
   },
 
@@ -69,31 +68,21 @@ function responseMapping(promotionEntries,requestHeader) {
   let mapping = [];
   promotionEntries.map((value, index) => {
     mapping.push({
-      "id": value.id,
-      "position": value.position,
-      "title": value.title,
+      "promotion_id": value.id,
+      'promotion_title': value.promotion_title,
       "photo_path": value.photo_path ? value.photo_path.url : '',
       "promotion_type": !value.wp_promotion_type ? '' : value.wp_promotion_type.type_title,
-      "category": !value.wp_category ? {} : value.wp_category,
-      // "category_id": !value.wp_category ? '' : value.wp_category.id,
-      // "category_title": !value.wp_category ? '' : value.wp_category.category_title,
-      "include_header": value.include_header,
-      "is_login": value.is_login,
-      "display": value.display,
-      "kyc_level_check":value.kyc_level_check,
-      "deep_link": value.wp_deeplink, 
-      "feature_id": !value.wp_feature_id ? null : value.wp_feature_id.feature_id, 
+      "category_id": !value.wp_category ? '' : value.wp_category.id,
+      "category_title": !value.wp_category ? '' : value.wp_category.category_title,
       "hasDetails": value.hasDetails,
       "action_link_ios": value.action_link_ios,
       "action_link_android": requestHeader.versioncode ? value.new_action_link_android : value.action_link_android,
+      "position": value.position,
       "promotion_code": value.promotion_code,
       "is_external": value.is_external,
       "external_deeplink": value.external_deeplink,
       "promotion_details": value.promotion_details,
-      "paths": value.paths,
-      "parameters": value.parameters,
-      "widget_id": !value.wp_home_widget ? null : value.wp_home_widget.id,
-      
+      "kyc_level_check":value.kyc_level_check
     });
   });
   return mapping;
--- src/api/wp-promotion/services/wp-promotion.js
+++ src/api/wp-promotion/services/wp-promotion.js
@@ -10,34 +10,20 @@ module.exports = createCoreService(
   "api::wp-promotion.wp-promotion",
   ({ strapi }) => ({
     async find(ctx) {
-      const promotions = await strapi.db.query("api::wp-promotion.wp-promotion").findMany(
-        
+      const promotions = await strapi.entityService.findMany(
+        "api::wp-promotion.wp-promotion",
         {
           populate: {
             photo_path: {
               select: ["url"],
             },
-            title: true,
-            wp_category: {
-              select: ["id", "name"],
-            },
+            wp_category: true,
             wp_promotion_type: true,
-            wp_deeplink: {
-              select: ["name", "deeplink", "is_external", "is_webURL", "alternative_url", "alternative_url_IOS", "deeplink_IOS", "client_id", "deeplink_id"]
-            },
-            wp_feature_id: {
-              select: ["feature_id"]
-            },
-            paths: true,
-            parameters: true,
-            wp_home_widget: {
-              select: ["id"]
-            },
           },
 
           publicationState: "live",
           sort: { position: "asc" },
-          where: {
+          filters: {
             $and: [
               {
                 wp_category: {
