3d384b6321a0ba2e9198301ad17080c284897d34
eizin <eizin.htun@wavemoney.com.mm>
2024-02-07 09:27:12 +0630

    add lifecycle for merchant blacklist

---
+++ src/api/merchant-blacklist/content-types/merchant-blacklist/lifecycle.js
@@ -0,0 +1,68 @@
+const axios = require('axios');
+
+const sendActionableMessage = (model, message) => {
+  const webhookURL = process.env.MS_WEBHOOK_URL;
+  let text = JSON.stringify(message);
+  axios.post(webhookURL, {
+    "themeColor": "0072C6",
+    "title": "Update feature blacklist to Middleware  ",
+    "text": `**Model** - ${model}  <br>**Message** - ${text}`,
+  }).then(function (response) {
+  }).catch(function (error) {
+  });
+};
+
+module.exports = {
+
+  async afterUpdate(event)  {
+    const { result, params, data } = event;
+    console.log("After Update");
+    console.log(result, 'Event data:');
+
+    const allData = await strapi.service('api::merchant-blacklist.merchant-blacklist').find(); 
+
+    console.log(allData, 'All Data');
+
+
+    axios.post(process.env.MW_MERCHANT_FEATURE_BLACKLIST_URL, allData
+    , {
+    }).then(function (response) {
+ 
+      console.log(response.data,'MW response');
+      sendActionableMessage('merchant-blacklist', response)
+
+    }).catch(function (error) {
+      console.log(error.response);
+      sendActionableMessage('merchant-blacklist', error.response)
+
+    });
+
+  },
+
+
+  async  afterDelete(event) {
+    const { result, params, data } = event;
+    console.log("After Delete");
+    console.log(result, 'Event data:');
+
+    const allData = await strapi.service('api::merchant-blacklist.merchant-blacklist').find(); 
+
+    console.log(allData, 'All Data');
+
+
+    axios.post(process.env.MW_MERCHANT_FEATURE_BLACKLIST_URL, allData
+    , {
+    }).then(function (response) {
+ 
+      console.log(response.data,'MW response');
+      sendActionableMessage('merchant-blacklist', response)
+
+    }).catch(function (error) {
+      console.log(error.response);
+      sendActionableMessage('merchant-blacklist', error.response)
+
+    });
+  },
+
+};
+
