c99fb0d4e1b57d33e8947c12365e52cc444e18ae
eizin <eizin.htun@wavemoney.com.mm>
2024-02-08 11:07:08 +0630

    delete console log

--- src/api/merchant-blacklist/content-types/merchant-blacklist/lifecycles.js
+++ src/api/merchant-blacklist/content-types/merchant-blacklist/lifecycles.js
@@ -16,14 +16,7 @@ module.exports = {
 
   async afterUpdate(event)  {
     const { result, params, data } = event;
-    console.log("After Update");
-    console.log(result, 'Event data:');
-
     const allData = await strapi.service('api::merchant-blacklist.merchant-blacklist').find(); 
-
-    console.log(allData, 'All Data');
-
-
     axios.post(process.env.MW_MERCHANT_FEATURE_BLACKLIST_URL, allData
     , {
       headers: {
@@ -32,8 +25,6 @@ module.exports = {
         'Content-Type': 'application/json'
       }
     }).then(function (response) {
- 
-      console.log(response.data,'MW response');
       sendActionableMessage('merchant-blacklist', response.data)
 
     }).catch(function (error) {
@@ -47,14 +38,8 @@ module.exports = {
 
   async  afterDelete(event) {
     const { result, params, data } = event;
-    console.log("After Delete");
-    console.log(result, 'Event data:');
-
+ 
     const allData = await strapi.service('api::merchant-blacklist.merchant-blacklist').find(); 
-
-    console.log(allData, 'All Data');
-
-
     axios.post(process.env.MW_MERCHANT_FEATURE_BLACKLIST_URL, allData
     , {
       headers: {
@@ -64,7 +49,7 @@ module.exports = {
       }
     }).then(function (response) {
  
-      console.log(response.data,'MW response');
+     
       sendActionableMessage('merchant-blacklist', response.data)
 
     }).catch(function (error) {
