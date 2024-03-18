4153a08eb78239ea5cc1e5a4d4a3ac7c8eaa9d3a
Htun Khaing Lynn <htunkhaing.lynn@wavemoney.com.mm>
2024-03-18 16:12:47 +0630

    implements callback to update data in MW

---
+++ src/api/wc-corporate-bank-list/content-types/wc-corporate-bank-list/lifecycles.js
@@ -0,0 +1,60 @@
+const axios = require("axios");
+
+const sendActionableMessage = (model, message) => {
+  const webhookURL = process.env.MS_WEBHOOK_URL;
+  let text = JSON.stringify(message);
+  axios
+    .post(webhookURL, {
+      themeColor: "0072C6",
+      title: "[WC] Update Corporate Bank List to Middleware",
+      text: `**Model** - ${model}  <br>**Message** - ${text}`,
+    })
+    .then(function (response) {})
+    .catch(function (error) {});
+};
+
+module.exports = {
+  async afterUpdate(event) {
+    const { result, params, data } = event;
+    const allData = await strapi
+      .service("api::wc-corporate-bank-list.wc-corporate-bank-list")
+      .find();
+    const requestData = JSON.stringify(allData);
+    axios
+      .post(process.env.MW_WC_CORPORATE_BANK_LIST_URL, requestData, {
+        headers: {
+          clientId: process.env.MW_WC_CLIENT_ID,
+          clientSecret: process.env.MW_WC_CLIENT_SECRET,
+          "Content-Type": "application/json",
+        },
+      })
+      .then(function (response) {
+        sendActionableMessage("wc-corporate-bank-list", response.data.message);
+      })
+      .catch(function (error) {
+        sendActionableMessage("wc-corporate-bank-list", error.data.message);
+      });
+  },
+
+  async afterDelete(event) {
+    const { result, params, data } = event;
+    const allData = await strapi
+      .service("api::wc-corporate-bank-list.wc-corporate-bank-list")
+      .find();
+    const requestData = JSON.stringify(allData);
+    axios
+      .post(process.env.MW_WC_CORPORATE_BANK_LIST_URL, requestData, {
+        headers: {
+          clientId: process.env.MW_WC_CLIENT_ID,
+          clientSecret: process.env.MW_WC_CLIENT_SECRET,
+          "Content-Type": "application/json",
+        },
+      })
+      .then(function (response) {
+        sendActionableMessage("wc-corporate-bank-list", response.data.message);
+      })
+      .catch(function (error) {
+        sendActionableMessage("wc-corporate-bank-list", error.data.message);
+      });
+  }
+};
--- src/api/wc-corporate-bank-list/services/wc-corporate-bank-list.js
+++ src/api/wc-corporate-bank-list/services/wc-corporate-bank-list.js
@@ -22,6 +22,23 @@ module.exports = createCoreService('api::wc-corporate-bank-list.wc-corporate-ban
         } catch (error) {
             throw new Error('Failed to fetch banks');
         }
+    },
+
+    async find(ctx) {
+        try {
+            const banks = await strapi.entityService.findMany(
+                'api::wc-corporate-bank-list.wc-corporate-bank-list',
+                {
+                    fields: ['uuid', 'name', 'account_no']
+                }
+            );
+            if (!banks) {
+                return ctx.notFound('Bank not found');
+            }
+            return banks;
+        } catch (error) {
+            throw new Error('Failed to fetch banks');
+        }
     }
 
 }));
