3dd0409ed6620c6e3234d8145c1824df8ed72c32
Kaung Khant Htun <kaungkhant.hutn@wavemoney.com.mm>
2024-04-01 18:20:35 +0700

    add find by feature id

--- src/api/wp-feature-id/controllers/wp-feature-id.js
+++ src/api/wp-feature-id/controllers/wp-feature-id.js
@@ -6,4 +6,13 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::wp-feature-id.wp-feature-id');
+module.exports = createCoreController('api::wp-feature-id.wp-feature-id', ({
+    async findFeatureId(ctx) {
+        const { featureId } = ctx.params;
+        let content = await strapi.service('api::wp-feature-id.wp-feature-id').findByFeatureId(featureId);
+        console.log(content);
+        return {
+            'contents': content
+        }
+     }
+}));
