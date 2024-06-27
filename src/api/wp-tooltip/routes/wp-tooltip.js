ebca19f3dcf3e2f201929c6217477fb39212b9f6
May Myat Mon <maymyatmon@DMM-Mac14GN2C4Y.local>
2024-06-27 16:08:07 +0630

    feature: febsibility check tooltops
    https://wavemoneytransform.atlassian.net/browse/PCT-784

---
+++ src/api/wp-tooltip/routes/wp-tooltip.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * wp-tooltip router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::wp-tooltip.wp-tooltip');
