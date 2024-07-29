4ffb68265e19c64dc52a034625e7264288e755fb
eizin <eizin.htun@wavemoney.com.mm>
2024-07-29 11:12:03 +0700

    remove merchant refund toggle

--- src/api/merchant-refund-toggle/routes/merchant-refund-toggle.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * merchant-refund-toggle router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::merchant-refund-toggle.merchant-refund-toggle');
