9dbdd11eeca24314a68f42884d1cdb2624759501
maymyat <maymyat.mon@wavemoney.com.mm>
2024-08-07 11:04:58 +0630

    fature:add feature reminder
    https://wavemoneytransform.atlassian.net/browse/DT-29

---
+++ src/api/wp-feature-reminder/controllers/wp-feature-reminder.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * wp-feature-reminder controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController(
+  "api::wp-feature-reminder.wp-feature-reminder",
+  ({ strapi }) => ({
+    async filterByVersion(ctx) {
+      let versionCode = ctx.query.versionCode;
+      let stories = await strapi
+        .service("api::wp-feature-reminder.wp-feature-reminder")
+        .getLatest(versionCode);
+      return {
+        data: stories,
+      };
+    },
+  })
+);
