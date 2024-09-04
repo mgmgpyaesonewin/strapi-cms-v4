b25d877c0abec3cda9dcb1416a9c5c093c1cf92a
maymyat <maymyat.mon@wavemoney.com.mm>
2024-09-04 13:25:43 +0630

    add cta button in pormotion -https://wavemoneytransform.atlassian.net/browse/DT-522

--- src/api/wp-promotions-ad/services/wp-promotions-ad.js
+++ src/api/wp-promotions-ad/services/wp-promotions-ad.js
@@ -1,129 +1,130 @@
-'use strict';
+"use strict";
 
 /**
  * wp-promotions-ad service
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::wp-promotions-ad.wp-promotions-ad');
 module.exports = createCoreService(
-    "api::wp-promotions-ad.wp-promotions-ad",
-    ({ strapi }) => ({
-      async find(ctx) {
-        const promotions = await strapi.entityService.findMany("api::wp-promotions-ad.wp-promotions-ad",
-          {
-            populate: {
-              photo_path: {
-                fields: ["url"],
-              },
-              title: true,
-              wp_category: {
-                fields: ["id", "name"],
-              },
-              wp_promotion_type: true,
-              wp_deeplink: {
-                fields: ["deeplink_id"],
-              },
-              wp_feature_id: {
-                fields: ["feature_id"]
-              },
-              paths: true,
-              parameters: true,
-              wp_home_widget: {
-                fields: ["id"]
-              },
-              sub_title: true,
-              description: true,
-              terms_and_conditions:true
+  "api::wp-promotions-ad.wp-promotions-ad",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const promotions = await strapi.entityService.findMany(
+        "api::wp-promotions-ad.wp-promotions-ad",
+        {
+          populate: {
+            photo_path: {
+              fields: ["url"],
             },
-  
-            publicationState: "live",
-            sort: { position: "asc" },
-            filters: {
-              $and: [
-                {
-                  wp_category: {
-                    id: {
-                      $notNull: true,
-                    },
+            title: true,
+            wp_category: {
+              fields: ["id", "name"],
+            },
+            wp_promotion_type: true,
+            wp_deeplink: {
+              fields: ["deeplink_id"],
+            },
+            wp_feature_id: {
+              fields: ["feature_id"],
+            },
+            paths: true,
+            parameters: true,
+            wp_home_widget: {
+              fields: ["id"],
+            },
+            sub_title: true,
+            description: true,
+            terms_and_conditions: true,
+            cta_button: true,
+          },
+
+          publicationState: "live",
+          sort: { position: "asc" },
+          filters: {
+            $and: [
+              {
+                wp_category: {
+                  id: {
+                    $notNull: true,
                   },
                 },
-                {
-                  wp_category: {
-                    publishedAt: {
-                      $notNull: true,
-                    },
+              },
+              {
+                wp_category: {
+                  publishedAt: {
+                    $notNull: true,
                   },
                 },
-              ],
-            },
-          }
-        );
-        return promotions;
-      },
-      async findOne(id) {
-        const promotion = await strapi.entityService.findOne(
-          "api::wp-promotions-ad.wp-promotions-ad",
-          id,
-          {
-            populate: {
-              photo_path: {
-                select: ["url"],
               },
-              wp_category: true,
-              wp_promotion_type: true,
-              sub_title: true,
-              description: true,
-              terms_and_conditions:true
+            ],
+          },
+        }
+      );
+      return promotions;
+    },
+    async findOne(id) {
+      const promotion = await strapi.entityService.findOne(
+        "api::wp-promotions-ad.wp-promotions-ad",
+        id,
+        {
+          populate: {
+            photo_path: {
+              select: ["url"],
             },
-            publicationState: "live",
-            filters: {
-              wp_category: {
-                id: {
-                  $notNull: true,
+            wp_category: true,
+            wp_promotion_type: true,
+            sub_title: true,
+            description: true,
+            terms_and_conditions: true,
+          },
+          publicationState: "live",
+          filters: {
+            wp_category: {
+              id: {
+                $notNull: true,
+              },
+            },
+          },
+        }
+      );
+      return promotion;
+    },
+    async filterByCategoryID(id) {
+      const promotion = await strapi.entityService.findMany(
+        "api::wp-promotions-ad.wp-promotions-ad",
+        {
+          populate: {
+            photo_path: {
+              select: ["url"],
+            },
+            wp_category: true,
+            wp_promotion_type: true,
+            sub_title: true,
+            description: true,
+            terms_and_conditions: true,
+          },
+          publicationState: "live",
+          sort: { position: "asc" },
+          filters: {
+            $and: [
+              {
+                wp_category: {
+                  id: id,
                 },
               },
-            },
-          }
-        );
-        return promotion;
-      },
-      async filterByCategoryID(id) {
-        const promotion = await strapi.entityService.findMany(
-          "api::wp-promotions-ad.wp-promotions-ad",
-          {
-            populate: {
-              photo_path: {
-                select: ["url"],
-              },
-              wp_category: true,
-              wp_promotion_type: true,
-              sub_title: true,
-              description: true,
-              terms_and_conditions:true
-            },
-            publicationState: "live",
-            sort: { position: "asc" },
-            filters: {
-              $and: [
-                {
-                  wp_category: {
-                    id: id,
+              {
+                wp_category: {
+                  publishedAt: {
+                    $notNull: true,
                   },
                 },
-                {
-                  wp_category: {
-                    publishedAt: {
-                      $notNull: true,
-                    },
-                  },
-                },
-              ],
-            },
-          }
-        );
-        return promotion;
-      },
-    })
-  );
+              },
+            ],
+          },
+        }
+      );
+      return promotion;
+    },
+  })
+);
