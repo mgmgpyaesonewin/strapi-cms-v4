6fc93114896d341f4ff76fdde45b3e1108dc1427
maymyat <maymyat.mon@wavemoney.com.mm>
2024-09-11 12:42:01 +0630

    add chinese localization -https://wavemoneytransform.atlassian.net/browse/DT-344

--- src/api/wp-localization-content/controllers/wp-localization-content.js
+++ src/api/wp-localization-content/controllers/wp-localization-content.js
@@ -1,25 +1,54 @@
-'use strict';
+"use strict";
 
 /**
  * wp-localization-content controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::wp-localization-content.wp-localization-content', ({ strapi }) => ({
-    
+module.exports = createCoreController(
+  "api::wp-localization-content.wp-localization-content",
+  ({ strapi }) => ({
     async find(ctx) {
-        let contents= await strapi.service('api::wp-localization-content.wp-localization-content').find(ctx);
-        return {
-            'contents':contents
-        }
-    },
-    async findKey(ctx) {
-        const { key } = ctx.params;
-        let content = await strapi.service('api::wp-localization-content.wp-localization-content').findByKey(key);
-        return {
-            'contents': content
-        }
-    }
+      const localizationService = strapi.service(
+        "api::wp-localization-content.wp-localization-content"
+      );
 
-}));
+      // Fetch content based on whether 'language' is provided in the query
+      let contents = ctx.query.language
+        ? await localizationService.findByLanguage(ctx)
+        : await localizationService.find(ctx);
+
+      // Modify the content only if there is a 'language' query
+      if (ctx.query.language) {
+        contents = contents.map((item) => {
+          if (item.value) {
+            item.value.text =
+              item.value.en || item.value.my || item.value.zw || item.value.zh; // Set text based on available language
+            delete item.value.en;
+            delete item.value.my;
+            delete item.value.zw;
+            delete item.value.zh;
+          }
+          return item;
+        });
+
+        console.log(JSON.stringify(contents, null, 2)); // Correct logging of modified contents
+      }
+
+      return {
+        contents,
+      };
+    },
+
+    async findKey(ctx) {
+      const { key } = ctx.params;
+      let content = await strapi
+        .service("api::wp-localization-content.wp-localization-content")
+        .findByKey(key);
+      return {
+        contents: content,
+      };
+    },
+  })
+);
